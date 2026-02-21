5ced88bdd0936cb932d81072c4bed7de718658eb
Edgar Sotomayor <edgarchido@live.com.mx>
2026-02-21 14:29:40 -0600

    Agrego programa sencillo para la P3

---
+++ practicas/3/SotomayorEdgar/Script_sencillo.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import messagebox
+import sys
+import os
+import traceback as tb
+
+os.system('cls')
+
+def excepbox(exc_type, exc_value, exc_traceback):
+    tb.print_exception(exc_type, exc_value, exc_traceback)
+    root = tk.Tk()
+    root.withdraw()
+    messagebox.showinfo(
+        "ERROR",
+        f"Se encontró un error de tipo {exc_type.__name__} : {exc_value}.\n"
+        "Toma una captura de pantalla del error"
+    )
+
+sys.excepthook = excepbox
+
+root = tk.Tk()
+root.withdraw()
+messagebox.showinfo("AVISO", "Fin del programa")
