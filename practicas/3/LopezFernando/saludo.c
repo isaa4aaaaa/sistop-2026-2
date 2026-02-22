edfe86276baebff5600af970be5abd936a20b8ca
theuniquefersa <holafersa@gmail.com>
2026-02-22 13:41:16 -0600

    Añado solicitud de nombre y su impresión al saludo

--- practicas/3/LopezFernando/saludo.c
+++ practicas/3/LopezFernando/saludo.c
@@ -3,8 +3,17 @@ López Morales Fernando Samuel
 Sistemas Operativos | 2026-2
 */
 #include <stdio.h>
-
+#include <string.h>
+#define MAX_S 50
 int main(){
-	printf("¡Hola mundo!\n");
+	char cadena[MAX_S];
+	printf("Ingresa tu nombre: ");
+	fgets(cadena, MAX_S, stdin);
+	int tam = -1;
+	for(int i = 0; i<MAX_S; i+=1){
+		if(cadena[i] == '\0' && tam == -1) tam = i;
+	}
+	cadena[tam-1] = '\0';
+	printf("¡Hola %s!\n", cadena);
 	return 0;
 }
