0b9ac85f5c0c05afa5babf1142b10579862d9c60
Bruno Ponce de León Reyes <br1pdlr@gmail.com>
2026-02-22 15:15:41 -0600

    Modifico el programa de saludo para obtener nombre del usuario

--- practicas/3/PoncedeleónBruno/saludoBruno.c
+++ practicas/3/PoncedeleónBruno/saludoBruno.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+char usuario[15];
+
 int main (void){
 
-	printf("\nHola mundo, soy Bruno :D\n");
+	printf("\nFavor de ingresar tu usuario\n");
+	scanf("%s",&usuario);
+	printf("\nHola %s :D\n", usuario);
 	return 0;
 }
