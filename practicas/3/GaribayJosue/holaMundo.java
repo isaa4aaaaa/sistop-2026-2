0ace08e0679842a647a37a439fa408cc61463110
Josue Garibay <josueclassfi@gmail.com>
2026-02-21 20:50:13 -0600

    Se modifico el archivo Java para hacer un saludo personal

--- practicas/3/GaribayJosue/holaMundo.java
+++ practicas/3/GaribayJosue/holaMundo.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class holaMundo{
     public static void main(String[]args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nombre = teclado.nextLine();
 
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola " + nombre + "!");
 
+        teclado.close();
     }
 }
