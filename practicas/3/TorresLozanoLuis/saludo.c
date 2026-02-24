e611ada0c2e6a8ede4bd4d2ccbeb7b7f34ee9fcf
Luis Torres Lozano <luistorres.lt716@gmail.com>
2026-02-24 13:47:10 -0600

    Agrego mi código de saludo.c modificado

--- practicas/3/TorresLozanoLuis/saludo.c
+++ practicas/3/TorresLozanoLuis/saludo.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola Mundo\n");
+    char nombre[50];
+
+    printf("Ingresa tu nombre:\n");
+    scanf("%49s", nombre); 
+    
+    printf("Hola %s\n", nombre);
     return 0;
 }
