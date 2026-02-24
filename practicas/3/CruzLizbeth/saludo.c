aa59f432a32a5130b1a570adba27c64eef5354bd
Milizgramo28 <manriquezliz@gmail.com>
2026-02-24 14:19:43 -0600

    Modificación del programa saludo.c

--- practicas/3/CruzLizbeth/saludo.c
+++ practicas/3/CruzLizbeth/saludo.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main () {
+	char nombre [50];
+
 	printf("Hola Mundo\n");
+	printf("¿Cuál es tu nombre? ");
+	scanf("%49s", nombre);
+	printf("Mucho gusto, %s!\n", nombre);
+
 	return 0;
 }
 
