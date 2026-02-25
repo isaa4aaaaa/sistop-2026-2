e35788fa6aa3a4b1b374c5bad8e4d8ab47f554d4
jesusalemonroy <jesus.monroy@ingenieria.unam.edu>
2026-02-24 18:08:27 -0600

    Practica 3 MonroyJesus ArchivoModificado

--- practicas/3/holaAle.c
+++ practicas/3/holaAle.c
@@ -3,7 +3,16 @@
 int main()
 {
 
-	printf("Hola Mundo soy Alejandro Monroy \n");
+	char nombre[20];
+
+	printf("Hola soy la version modificada \n");
+	printf("Ingresa tu nombre o un nombre por favor\n");
+	scanf("%s", &nombre);
+	printf("\n Hola %s \t", nombre);
+
+	printf("\nEl mundo te saluda");
+
+	printf("\nTuviste un buen dia?");
 
 
 	return 0;
