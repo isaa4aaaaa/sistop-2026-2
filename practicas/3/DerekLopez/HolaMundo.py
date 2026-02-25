8b0618d75a5d478c6466e7a08415037ce9da13ab
Derek Andre Lopez Granados <derlogran@gmail.com>
2026-02-24 21:44:58 -0600

    Se modifico el archivo HolaMundo.py para realizar un saludo personalizado

--- practicas/3/DerekLopez/HolaMundo.py
+++ practicas/3/DerekLopez/HolaMundo.py
@@ -1,1 +1,10 @@
-print(" Hola Mundo")
+import sys
+
+def main():
+    if len(sys.argv)>1:
+        print(f"Hola, {sys.argv[1]}")
+    else:
+        print("Hola,usuario")
+
+if __name__ == "__main__":
+    main()
