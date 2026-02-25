385cbc6189cf5a39873d550775294c9cc71ada81
jesusalemonroy <jesus.monroy@ingenieria.unam.edu>
2026-02-24 18:25:39 -0600

    Movi archivos a carpeta correcta

---
+++ practicas/3/MonroyJesus/holaAle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+	char nombre[20];
+
+	printf("Hola soy la version modificada \n");
+	printf("Ingresa tu nombre o un nombre por favor\n");
+	scanf("%s", &nombre);
+	printf("\n Hola %s \t", nombre);
+
+	printf("\nEl mundo te saluda");
+
+	printf("\nTuviste un buen dia?");
+
+
+	return 0;
+}
