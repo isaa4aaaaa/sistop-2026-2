2e15546c94a55cdf699f630be317c247b6116815
Fredy Emiliano Navarro Carbajal <fredynavarrocarbajal@gmail.com>
2026-02-24 20:27:03 -0600

    Modificacion del archivo  Hola, en el lenguaje C++

--- practicas/3/FredyNavarro/Hola.cpp
+++ practicas/3/FredyNavarro/Hola.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    cout << "Helloooo :) \n";
+    string name;
+    cout << "Escribe tu nombre: ";
+    cin >> name;
+    cout << "Helloooo :) " << name << "\n";
     return  0;
 }
